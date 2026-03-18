757603629f2dd792b16e00b61f7fc2c2d20d65db
Leqi Li <leqi.li-3@student.manchester.ac.uk>
2026-03-18 17:27:40 +0000

    boolean_cal

--- lab-k/boolean_cal.c
+++ lab-k/boolean_cal.c
@@ -2,14 +2,14 @@
 int main(void) {
 
   // Declare variables
-  bool x, y, a1, a2, a;
+  bool x, y, a1, a2, a, z;
 
   // Using the variables
   x = 0;
   y = 0;
   a1 = x || y; // or
   a2 = x && y; // and
-  a = (NOT(x) OR(y AND z))AND NOT(y);
+  a = (!(x) || (y && z)) && !y;
 
   return 0;
 }
