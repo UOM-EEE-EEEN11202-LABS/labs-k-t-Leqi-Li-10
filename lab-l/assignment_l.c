2879eb29b57f88a5357fbba67dcb94f6a0a44467
Leqi Li <leqi.li-3@student.manchester.ac.uk>
2026-03-18 23:44:59 +0000

    assignment

--- lab-l/assignment_l.c
+++ lab-l/assignment_l.c
@@ -6,11 +6,26 @@ int main(void) {
   scanf("%d", &n);
 
   // Your code in this section
+  char my_email[] = "leqi.li-3@student.manchester.ac.uk";
+  int vowel = 0;
+  int email_len = 0;
 
+  while (my_email[email_len] != '\0') {
+    email_len++;
+  }
 
-  printf("Yes!"); // place as needed in the code. You can use more than once.
-  printf("No!"); // place as needed in the code. You can use more than once.
-  printf("Error"); // place as needed in the code. You can use more than once.
+  if (n < 0 || n >= email_len) {
+    printf("Error"); // place as needed in the code. You can use more than once.
+  } else {
+    char target = my_email[n];
+    if (target == 'a' || target == 'e' || target == 'i' || target == 'o' ||
+        target == 'u') {
+      printf(
+          "Yes!"); // place as needed in the code. You can use more than once.
+    } else {
+      printf("No!"); // place as needed in the code. You can use more than once.
+    }
+  }
 
   // Do not edit below here
   return 0;
