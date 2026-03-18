a536179f4e7847d2f77df82dbb1006f89dfcca31
Leqi Li <leqi.li-3@student.manchester.ac.uk>
2026-03-18 22:06:42 +0000

    assignment

--- lab-k/assignment_k.c
+++ lab-k/assignment_k.c
@@ -6,6 +6,20 @@ int main(void) {
   scanf("%lf", &height);
 
   // Your code in this section
+  // Declare variables
+  const double PI = 3.1415926;
+  const char *email = "leqi.li-3@student.manchester.ac.uk";
+  double volume = 0.0;
+  const double r = 2.2;
+
+  // Set variables
+  // Calculate the volume
+  // Check for invalid input
+  if (height <= 0) {
+    printf("Error\n");
+    return 1;
+  }
+  volume = PI * r * r * height;
 
   // Do not edit below here
   printf("%s\n", email);
